09756f905a4877ec530fb586a27bb55ac43e2fea
Tenotonor <148351901+Tenotonor@users.noreply.github.com>
2024-02-29 23:06:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
 // Store instruments data
 let instruments = {};
 
