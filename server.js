b0b41f98aa1cec9a2b047b280b3ff7b7a1903d57
Tenotonor <148351901+Tenotonor@users.noreply.github.com>
2024-02-29 22:57:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,13 @@ let instruments = {};
 
 // Serve the main page
 app.get('/', (req, res) => {
+    console.log('Serving index.html');
     res.sendFile(__dirname + '/index.html');
 });
 
 // Serve the JavaScript file
 app.get('/script.js', (req, res) => {
+    console.log('Serving script.js');
     res.sendFile(__dirname + '/script.js');
 });
 
@@ -37,7 +39,14 @@ io.on('connection', (socket) => {
 
 // Handle data from C# client
 app.post('/log', (req, res) => {
+    console.log('Received POST request to /log with data:', req.body);
+
     const { instName, userName, ipAddr } = req.body;
+    if (!instName || !userName || !ipAddr) {
+        console.error('Missing data in request:', req.body);
+        return res.status(400).json({ error: 'Missing data in request' });
+    }
+
     // Assuming instName is unique for each instrument/client
     const key = instName;
 
@@ -47,6 +56,8 @@ app.post('/log', (req, res) => {
         ipAddr,
         lastUpdated: new Date().toISOString() // Adding a timestamp for the last update
     };
+
+    console.log(`Updated instruments data: ${JSON.stringify(instruments)}`);
 
     // Emit updated instruments data to all clients
     io.emit('update', instruments);
