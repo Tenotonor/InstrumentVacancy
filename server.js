7ffe4511845f431b854650b5f2645fa924e249ea
Tenotonor <148351901+Tenotonor@users.noreply.github.com>
2024-03-06 14:56:08 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,15 +25,17 @@ io.on('connection', (socket) => {
 // Log data route
 app.get('/log', (req, res) => {
     const { instName, userName, ipAddr } = req.query;
+    const lastUpdateTime = new Date().toLocaleString(); // Get current time as a readable string
 
-    // Update instruments data
-    instrumentsData[instName] = { instName, userName, ipAddr };
+    // Update instruments data with the last update time
+    instrumentsData[instName] = { instName, userName, ipAddr, lastUpdateTime };
 
     // Emit to all connected clients
-    io.emit('update', { instName, userName, ipAddr });
+    io.emit('update', instrumentsData[instName]);
 
     res.send('Data received successfully');
 });
+
 
 http.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
