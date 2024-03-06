a6abc95924c0564250e5135fa60a895775e8c94d
Tenotonor <148351901+Tenotonor@users.noreply.github.com>
2024-03-06 14:21:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,45 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const cors = require('cors');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "*", // Allows access from any origin
+    methods: ["GET", "POST"] // Allows only GET and POST requests
+  }
+});
+
 const port = process.env.PORT || 8080;
+
+// Middleware to solve CORS issues when your client is on a different origin
+app.use(cors());
 
 // Serve static files from the root directory
 app.use(express.static('.'));
 
-// WebSocket connection
+// WebSocket connection handling
 io.on('connection', (socket) => {
-    console.log('A user connected');
-    
+    console.log('A user connected via Socket.IO');
+
+    // This can still be used to send an initial message to just the connected client
+    socket.emit('update', { instName: 'Initial', userName: 'TestUser', ipAddr: '127.0.0.1' });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
 });
 
-// Log data route
+// Route for logging data and emitting updates to all connected clients
 app.get('/log', (req, res) => {
-    // Destructure and default values to ensure no undefined fields
-    const { instName = 'N/A', userName = 'N/A', ipAddr = 'N/A' } = req.query;
-
+    const { instName, userName, ipAddr } = req.query;
     console.log(`Received data: instName=${instName}, userName=${userName}, ipAddr=${ipAddr}`);
 
-    // Construct the data object carefully, ensuring no undefined values
-    const dataToSend = {
-        instName,
-        userName,
-        ipAddr
-    };
-
-    // Emit to all connected clients
-    io.emit('update', dataToSend);
-    console.log(`io.emit update called successfully with:`, dataToSend);
+    // Emitting to all connected clients
+    io.emit('update', { instName, userName, ipAddr });
+    console.log('io.emit update called successfully with:', { instName, userName, ipAddr });
 
     res.send('Data received successfully');
 });
-
 
 http.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
