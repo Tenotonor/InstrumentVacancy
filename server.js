fce4a77529184acfd5f96b46c163d3ebbd0a0599
Tenotonor <148351901+Tenotonor@users.noreply.github.com>
2024-03-06 14:03:37 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,22 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 8080;
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); // Serve static files from 'public' directory
-
-// Main page route
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+// Serve static files from the root directory
+app.use(express.static('.'));
 
 // WebSocket connection
-//io.on('connection', (socket) => {
-//    console.log('A user connected');
-//    socket.on('disconnect', () => {
-//        console.log('User disconnected');
-//   });
-//});
-
 io.on('connection', (socket) => {
     console.log('A user connected');
-    // Immediately emit an event to this client
-    socket.emit('update', { instName: 'Test', userName: 'TestUser', ipAddr: '127.0.0.1' });
-});
+    // Emit an initial message to this client
+    socket.emit('update', { instName: 'Initial', userName: 'TestUser', ipAddr: '127.0.0.1' });
 
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 // Log data route
 app.get('/log', (req, res) => {
@@ -36,26 +25,10 @@ app.get('/log', (req, res) => {
 
     // Emit to all connected clients
     io.emit('update', { instName, userName, ipAddr });
-    console.log(`io.emit update called successfully`);
+    console.log('io.emit update called successfully');
 
     res.send('Data received successfully');
-    console.log(`Data received successfully`);
 });
-
-app.get('/events', function(req, res) {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-
-    // a function to send messages
-    const sendUpdate = (data) => {
-        res.write(`data: ${JSON.stringify(data)}\n\n`);
-    };
-
-    sendUpdate({ message: "Initial message" });
-
-    // You could call sendUpdate whenever there's new data to send to the client
-});
-
 
 http.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
